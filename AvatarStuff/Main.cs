6adb01744e44f673bb3961fb97c816fc91d2509a
Katistix <33645731+Katistic@users.noreply.github.com>
2020-06-30 12:00:34 +1000

    Source Project

---
+++ AvatarStuff/Main.cs
@@ -0,0 +1,198 @@
+﻿using MelonLoader;
+using System;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC;
+using VRC.Core;
+using VRC.UI;
+
+namespace AvatarStuff
+{
+    public class Main : MelonMod
+    {
+        public static Player me;
+        public static bool emmCheck = false;
+        public static float lastVal = 0f;
+        public static float lastRotation = 0f;
+        public static bool skipRotationFix = false;
+        public static GameObject baseButton;
+
+        public static void Log(string log, bool debug=false)
+        {
+            if (debug)
+            {
+                if (!Imports.IsDebugMode()) return;
+                log = "[DEBUG] " + log;
+            }
+            MelonModLogger.Log(log);
+        }
+
+        public static void WarnLog(string log, bool debug = false)
+        {
+            if (debug)
+            {
+                if (!Imports.IsDebugMode()) return;
+                log = "[DEBUG] " + log;
+            }
+            MelonModLogger.LogWarning(log);
+        }
+
+        public static void ErrLog(string log, bool debug = false)
+        {
+            if (debug)
+            {
+                if (!Imports.IsDebugMode()) return;
+                log = "[DEBUG] " + log;
+            }
+            MelonModLogger.LogError(log);
+        }
+
+        public override void VRChat_OnUiManagerInit()
+        {
+            string DoubleStandards = "emmVRC can be obfuscated but this can't be? Thats a bit shitty.";
+
+            baseButton = Sub.GetUiObject("/Avatar/Stats Button");
+
+            GameObject listObj = Sub.CreateObjectFrom(Sub.GetUiObject("/Avatar").transform.Find("Vertical Scroll View/Viewport/Content").transform.Find("Favorite Avatar List").gameObject);
+            listObj.transform.Find("Button").GetComponentInChildren<Text>().text = "Public Avatar List";
+            listObj.transform.SetSiblingIndex(0);
+            listObj.SetActive(false);
+
+            UiAvatarList listListObj = listObj.GetComponent<UiAvatarList>();
+            listListObj.category = (UiAvatarList.EnumNPublicSealedvaInPuMiFaSpClPuLi9vUnique)4;
+            listListObj.StopAllCoroutines();
+
+            Sub.CreateAvatarButton("Get Public Avatars", -(baseButton.GetComponent<RectTransform>().sizeDelta.x) - 10f, 0f, delegate ()
+            {
+                Log("Get Public Avatars button (Avatar Menu) clicked!", true);
+
+                ApiAvatar currentAvatar = listListObj.avatarPedestal.field_Internal_ApiAvatar_0;
+                System.Collections.Generic.List<ApiAvatar> avatars = Sub.GetPublicAvatars(currentAvatar.authorId);
+
+                MelonModLogger.Log("Got " + avatars.Count.ToString() + " Public Avatars for user " + currentAvatar.authorName);
+
+                listObj.transform.Find("Button").GetComponentInChildren<Text>().text = "Public Avatars for user " + currentAvatar.authorName;
+                listObj.SetActive(true);
+                listListObj.field_Private_Dictionary_2_String_ApiAvatar_0.Clear();
+
+                string[] arr = (from avatar in avatars select avatar.id).ToArray();
+                foreach (ApiAvatar avatar in avatars) listListObj.field_Private_Dictionary_2_String_ApiAvatar_0.Add(avatar.id, avatar);
+                listListObj.specificListIds = arr;
+                listListObj.Method_Protected_Abstract_Virtual_New_Void_Int32_0(0);
+            }, false, "/Avatar/Favorite Button", 300f);
+
+            Sub.CreateAvatarButton("Random Public Avatar", -(baseButton.GetComponent<RectTransform>().sizeDelta.x) - 10f, 80f, delegate ()
+            {
+                Log("Random Public Avatar button clicked!", true);
+
+                Sub.SwitchPedestalToRandomPublicAvatar();
+            }, false, "/Avatar/Favorite Button", 300f);
+
+            Button SocialAvatarButton = new Button(); // Placehold
+            GameObject SocialAvatarObj;
+
+            (SocialAvatarButton, SocialAvatarObj) = Sub.CreateSocialButton("Get Public Avatars", 215f, 19f, delegate ()
+            {
+                Log("Get Public Avatars button (Social Menu) clicked!", true);
+
+                PageUserInfo userInfo = GameObject.Find("Screens").transform.Find("UserInfo").transform.GetComponentInChildren<PageUserInfo>();
+                System.Collections.Generic.List<ApiAvatar> avatars = Sub.GetPublicAvatars(userInfo.user.id);
+
+                MelonModLogger.Log("Got " + avatars.Count.ToString() + " Public Avatars for user " + userInfo.user.displayName);
+
+                if (avatars.Count == 0)
+                {
+                    Sub.DoErrorPopup("Public Avatars for " + userInfo.user.displayName, "No public avatars were found for " + userInfo.user.displayName);
+                    return;
+                }
+
+                listObj.transform.Find("Button").GetComponentInChildren<Text>().text = "Public Avatars for user " + userInfo.user.displayName;
+                listObj.SetActive(true);
+                listListObj.field_Private_Dictionary_2_String_ApiAvatar_0.Clear();
+
+                string[] arr = (from avatar in avatars select avatar.id).ToArray();
+                foreach (ApiAvatar avatar in avatars) listListObj.field_Private_Dictionary_2_String_ApiAvatar_0.Add(avatar.id, avatar);
+                listListObj.specificListIds = arr;
+                listListObj.Method_Protected_Abstract_Virtual_New_Void_Int32_0(0);
+
+                // Switch to avatar page
+                VRCUiManager.prop_VRCUiManager_0.Method_Public_VRCUiPage_VRCUiPage_0(Sub.GetUiObject("/Avatar").gameObject.GetComponentInChildren<VRCUiPage>());
+            }, xsize: 200f);
+
+            Sub.CreateSlider(Sub.GetUiObject("/Avatar/Stats Button").transform.parent, baseButton.transform.localPosition.x, baseButton.transform.localPosition.y + (80f*3), baseButton.GetComponent<RectTransform>().sizeDelta.x, 0f, 360f, delegate(float val)
+            {
+                Log("Slider value changed to " + val.ToString(), true);
+                try
+                {
+                    Transform tf = GameObject.Find("Screens").transform.Find("Avatar").GetComponent<PageAvatar>().avatar.field_Private_GameObject_0.transform;
+
+                    tf.localRotation = Quaternion.Euler(new Vector3(0, tf.localRotation.eulerAngles.y + (val - lastVal), 0));
+
+                    lastVal = val;
+                    skipRotationFix = true;
+                } 
+                catch (Exception e)
+                {
+                    WarnLog("Failed to change avatar preview rotation.", true);
+                }
+            }, 180f);
+
+            emmCheck = true;
+        }
+
+        public override void OnUpdate()
+        {
+            // emmVRC compatability
+            if (emmCheck)
+            {
+                ApiWorld currentRoom = RoomManagerBase.field_Internal_Static_ApiWorld_0;
+                if (currentRoom != null && currentRoom.id != "")
+                {
+                    emmCheck = false;
+                    return;
+                }
+
+                Button[] buttons = Sub.GetUiObject("/Avatar/").GetComponentsInChildren<Button>();
+
+                foreach (Button button in buttons)
+                {
+                    if (button.name == "Favorite Button(Clone)" && (button.GetComponentInChildren<Text>().text.Contains("Favorite") || button.GetComponentInChildren<Text>().text.Contains("Unfavorite")) && button.gameObject.transform.localPosition == new Vector3(-561f, -227f, -2f))
+                    {
+                        if (button.gameObject.transform.localPosition == new Vector3(-561f, -227f, -2f))
+                        {
+                            WarnLog("Found non-compatable emmVRC install, doing compatability changes.");
+
+                            Sub.ChangeObjectPositionBy(button.gameObject, -(baseButton.GetComponent<RectTransform>().sizeDelta.x) - 10f, 0f);
+                            button.gameObject.transform.GetComponent<RectTransform>().sizeDelta = new Vector2(300f, button.gameObject.transform.GetComponent<RectTransform>().sizeDelta.y);
+                        }
+                        emmCheck = false;
+                        return;
+                    }
+                }
+            }
+
+            try
+            {
+                GameObject tf = GameObject.Find("Screens").transform.Find("Avatar").GetComponent<PageAvatar>().avatar.field_Private_GameObject_0;
+
+                if (tf.transform.localPosition.y < 70f || tf.transform.localPosition.y > 75) // Avatar Preview Position Adjustment
+                {
+                    tf.transform.localPosition = new Vector3(tf.transform.localPosition.x, 70f, tf.transform.localPosition.z);
+                }
+
+                if (!skipRotationFix) // Avatar Preview Anti-Rotation
+                {
+                    tf.transform.rotation = Quaternion.Euler(new Vector3(tf.transform.rotation.eulerAngles.x, lastRotation, tf.transform.rotation.eulerAngles.z));
+                }
+                else skipRotationFix = false;
+
+                lastRotation = tf.transform.rotation.eulerAngles.y;
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
---
+++ AvatarStuff/Sub.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Linq;
+using System.Net.Security;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Security.Cryptography.X509Certificates;
+
+using Newtonsoft.Json;
+
+using MelonLoader;
+using UnityEngine;
+using UnityEngine.UI;
+using UnhollowerRuntimeLib;
+
+using VRC.UI;
+using VRC.Core;
+
+namespace AvatarStuff
+{
+    class Sub
+    {
+
+        // Helper methods
+        public static void ChangeObjectPositionBy(GameObject obj, float x, float y)
+        {
+            obj.transform.localPosition += new Vector3(x, y);
+        }
+
+        public static void SwitchPedestalToAvatar(ApiAvatar avatar, bool confirm = false)
+        {
+            PageAvatar menu = GameObject.Find("Screens").transform.Find("Avatar").GetComponent<PageAvatar>();
+            UnityEngine.Object.Destroy(menu.avatar.field_Private_GameObject_0);
+            menu.avatar.Method_Public_Void_ApiAvatar_0(avatar);
+
+            if (confirm) menu.ChangeToSelectedAvatar(); //menu.avatar.field_Internal_ApiAvatar_0 = avatar;
+        }
+
+        public static GameObject GetUiObject(string path, bool full = false)
+        {
+            // Main.Log(full ? path : "/UserInterface/MenuContent/Screens" + path, true);
+            return GameObject.Find(full ? path : "/UserInterface/MenuContent/Screens" + path);
+        }
+
+        // Ui Creators
+        public static GameObject CreateObjectFrom(GameObject obj, bool removeButtonIndicators = false, Transform parent = null)
+        {
+            obj = UnityEngine.Object.Instantiate<GameObject>(obj, parent ?? obj.transform.parent, true);
+
+            if (removeButtonIndicators)
+            {
+                foreach (Component image in obj.GetComponent<Button>().GetComponentsInChildren(Il2CppType.Of<Image>()))
+                {
+                    if (image.name.StartsWith("Icon_")) UnityEngine.Object.DestroyImmediate(image);
+                }
+            }
+
+            return obj;
+        }
+
+        public static GameObject CreateAvatarButton(string text, float x, float y, System.Action action, bool absolute = false, string baseButton = "/Avatar/Favorite Button", float xsize = 0f, float ysize = 0f)
+        {
+            GameObject publicAvatarButtonObject = CreateObjectFrom(GetUiObject(baseButton), true);
+            Button publicAvatarButton = publicAvatarButtonObject.GetComponent<Button>();
+
+            if (!absolute) publicAvatarButtonObject.transform.localPosition =
+                    new Vector3(publicAvatarButtonObject.transform.localPosition.x + x,
+                    publicAvatarButtonObject.transform.localPosition.y + y);
+            else publicAvatarButtonObject.transform.localPosition = new Vector3(x, y);
+
+            Vector2 sd = publicAvatarButtonObject.GetComponent<RectTransform>().sizeDelta;
+            publicAvatarButtonObject.GetComponent<RectTransform>().sizeDelta = new Vector2((xsize != 0) ? xsize : sd.x, (ysize != 0) ? ysize : sd.y);
+
+            publicAvatarButtonObject.GetComponentInChildren<Text>().text = text;
+            publicAvatarButton.onClick.RemoveAllListeners();
+            publicAvatarButton.onClick.AddListener(action);
+
+            return publicAvatarButtonObject;
+        }
+
+        public static Slider CreateSlider(Transform parent, float x, float y, float width, float min, float max, Action<float> action, float value = 0f)
+        {
+            Slider newSlider = GameObject.Instantiate(Resources.FindObjectsOfTypeAll<UIDeviceSelector>().FirstOrDefault(e => e.name == "AudioDevicePanel")?.transform.Find("VolumeSlider")?.GetComponent<Slider>(), parent);
+
+            newSlider.minValue = min;
+            newSlider.maxValue = max;
+            newSlider.value = value;
+            newSlider.transform.localPosition = new Vector3(x, y);
+            newSlider.GetComponent<RectTransform>().sizeDelta = new Vector2(width, newSlider.GetComponent<RectTransform>().sizeDelta.y);
+
+            newSlider.onValueChanged = new Slider.SliderEvent();
+            if (action != null) newSlider.onValueChanged.AddListener((Action<float>)(v => action(v)));
+
+            return newSlider;
+        }
+
+        public static (Button, GameObject) CreateSocialButton(string text, float x, float y, System.Action action, bool absolute = false, string baseButton = "/UserInfo/User Panel/Moderator/Actions/Warn", float xsize = 0f, float ysize = 0f)
+        {
+            Transform transform = GetUiObject("/UserInfo").gameObject.transform;
+            GameObject obj = CreateObjectFrom(GetUiObject(baseButton));
+            Button butt = obj.GetComponent<Button>();
+
+            if (!absolute) obj.transform.localPosition += new Vector3(x, y);
+            else obj.transform.localPosition.Set(x, y, obj.transform.localPosition.z);
+
+            Vector2 sd = obj.GetComponent<RectTransform>().sizeDelta;
+            obj.GetComponent<RectTransform>().sizeDelta = new Vector2((xsize != 0) ? xsize : sd.x, (ysize != 0) ? ysize : sd.y);
+            butt.GetComponent<RectTransform>().sizeDelta = new Vector2((xsize != 0) ? xsize : sd.x, (ysize != 0) ? ysize : sd.y);
+
+            obj.GetComponentInChildren<Text>().text = text;
+            butt.onClick = new Button.ButtonClickedEvent();
+            butt.onClick.AddListener(action);
+
+            butt.gameObject.SetActive(true);
+            butt.enabled = true;
+            butt.GetComponentInChildren<Image>().color = Color.green;
+
+            butt.transform.SetParent(transform);
+
+            return (butt, obj);
+        }
+
+        // 'Do' methods
+        public static void DoErrorPopup(string title, string content)
+        {
+            MelonModLogger.LogWarning(content);
+            VRCUiPopupManager.field_Private_Static_VRCUiPopupManager_0.Method_Public_Void_String_String_Single_3(title, content, 20f);
+        }
+
+        // 'Get' methods
+        public static List<ApiAvatar> GetPublicAvatars(string uId)
+        {
+            if (uId == null || !uId.StartsWith("usr_")) return null;
+
+            WebRequest request = WebRequest.Create("https://api.vrchat.cloud/api/1/avatars?apiKey=" + API.ApiKey + "&userId=" + uId + "&order=descending");
+            ServicePointManager.ServerCertificateValidationCallback = (System.Object s, X509Certificate c, X509Chain cc, SslPolicyErrors ssl) => true;
+            WebResponse response = request.GetResponse();
+
+            string result = "";
+            using (Stream rs = response.GetResponseStream())
+            {
+
+                using (StreamReader sr = new StreamReader(rs))
+                {
+                    result = sr.ReadToEnd();
+                }
+            }
+            response.Dispose();
+
+            List<Dictionary<string, dynamic>> list = JsonConvert.DeserializeObject<List<Dictionary<string, dynamic>>>(result);
+            List<ApiAvatar> avatarList = new List<ApiAvatar>();
+
+            List<string> listOThings = new List<string>()
+            {
+                "name", "id", "description", "authorId", "authorName", "imageUrl", "thumbnailImage", "assetUrl"
+            };
+
+            foreach (Dictionary<string, dynamic> dAvatar in list)
+            {
+                ApiAvatar avatar = new ApiAvatar();
+                foreach (string key in dAvatar.Keys)
+                {
+                    if (listOThings.Contains(key))
+                    {
+                        avatar.WriteField(key, dAvatar[key]);
+                    }
+                }
+
+                avatarList.Add(avatar);
+            }
+            return avatarList;
+        }
+
+        public static void SwitchPedestalToRandomPublicAvatar()
+        {
+            string id;
+
+            WebRequest request = WebRequest.Create("http://vrcavatars.tk/public/random");
+            ServicePointManager.ServerCertificateValidationCallback = (object s, X509Certificate c, X509Chain cc, SslPolicyErrors ssl) => true;
+            WebResponse response = request.GetResponse();
+
+            using (StreamReader rs = new StreamReader(response.GetResponseStream())) id = Regex.Replace(rs.ReadToEnd(), @"\n", "");
+
+            ApiAvatar avatar = new ApiAvatar();
+            avatar.id = id;
+
+            avatar.Get(new Action<ApiContainer>(delegate (ApiContainer container)
+            {
+                if (avatar.releaseStatus != "public")
+                {
+                    SwitchPedestalToRandomPublicAvatar();
+                    return;
+                }
+
+                SwitchPedestalToAvatar(avatar);
+            }));
+        }
+    }
+}
